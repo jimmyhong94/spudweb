6a2c77b3fec336d84276eacdcb9ac58a34f594bf
jimlitical <jimlitical@gmail.com>
2015-12-15 19:16:11 -0800

    Made a navigation file inside the includes folder. Required it in the index.

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
-<?php require_once "include/header.php"; ?>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">SPUD</a>
-    </div>
-    <div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-calendar"></span>EVENTS</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-folder-open"></span>PROJECTS</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-cloud"></span>ABOUT</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-plus"></span>JOIN</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span>SIGN UP</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>LOGIN</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php require "include/header.php"; ?>
+<?php require "include/navigation.php"; ?>
 
 <div class="jumbotron">
   <h1>SPU DEVELOPERS</h1>
