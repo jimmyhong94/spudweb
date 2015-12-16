299531dcb21f9910e112fd54b77ac703a15602c4
jimlitical <jimlitical@gmail.com>
2015-12-15 19:13:00 -0800

    Got rid of clutter. Created php files for better development. Include folder is for files such as the header (links, scripts, metadata, etc), navigation, and other consistently used files.

---
+++ include/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SPU Developers</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <style>
+  @font-face {
+      font-family: Lombok;
+      src: url(Lombok.otf);
+  }
+  .jumbotron h1 {
+    font-family: Lombok;
+  }
+  .jumbotron{
+    text-align: center;
+    background-image: url("48-ice.png");
+    background-size: cover;
+    margin-bottom: 0;
+  }
+  .btn{
+    border-radius: 0;
+  }
+  .navbar{
+    margin-bottom: 0;
+    border-radius: 0;
+  }
+  .middle{
+    background-image: url("49-Comet.png");
+    background-size: cover;
+    min-height: 500px;
+    margin-top: 0;
+    color: #F5F5F5;
+  }
+  .container h2{
+    text-align: center;
+  }
+  .glyphicon{
+    margin-right: 6px;
+  }
+  </style>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php require_once "include/header.html"; ?>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">SPUD</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-calendar"></span>EVENTS</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-folder-open"></span>PROJECTS</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-cloud"></span>ABOUT</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-plus"></span>JOIN</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span>SIGN UP</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>LOGIN</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>SPU DEVELOPERS</h1>
+  <p>Welcome Statement...</p>
+  <p><a class="btn btn-default btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+
+<div class="middle">
+  <div class="container">
+    <h2>Hello</h2>
+  </div>
+</div>
+
+</body>
+</html>
