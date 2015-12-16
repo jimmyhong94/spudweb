347b5242ce9f232e2069f0fd6ac75d367005b722
jimlitical <jimlitical@gmail.com>
2015-12-15 19:19:23 -0800

    Added files for main navigation buttons. Gave them all the navigation.

---
+++ include/navigation.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">SPUD</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-calendar"></span>EVENTS</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-folder-open"></span>PROJECTS</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-cloud"></span>ABOUT</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-plus"></span>JOIN</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span>SIGN UP</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>LOGIN</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
