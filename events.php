a04b597ec72ea7b100b0c682ed6805813459e8b3
jimlitical <jimlitical@gmail.com>
2015-12-17 04:44:34 -0800

    Enabled events from db to be shown properly with bootstrap grid. Need to add deletion on events that have past.

--- events.php
+++ events.php
@@ -7,45 +7,66 @@ $objDatabase = new DatabaseObj;
 $db = $objDatabase->Open();
 $query = "Select * from events;";
 $result = $db->query($query);
-?>
-<div class='row'>
-    <div class='col-md-1'>
-    </div>
-    <div class='col-xs-12 col-md-3'>
-        <div class='thumbnail'>
-            <img src='img/49-Comet.png' alt='...'>
-            <div class='caption'>
-                <h3>Event Name</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at dui eget ante sollicitudin posuere. </p>
-                <p><a href='#' class='btn btn-primary' role='button'>Details</a></p>
-            </div>
-        </div>
-    </div>
-    <div class='col-xs-12 col-md-3'>
-        <div class='thumbnail'>
-            <img src='img/49-Comet.png' alt='...'>
-            <div class='caption'>
-                <h3>Event Name</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at dui eget ante sollicitudin posuere. </p>
-                <p><a href='#' class='btn btn-primary' role='button'>Details</a></p>
-            </div>
-        </div>
-    </div>
-    <div class='col-xs-12 col-md-3'>
-        <div class='thumbnail'>
-            <img src='img/49-Comet.png' alt='...'>
-            <div class='caption'>
-                <h3>Event Name</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at dui eget ante sollicitudin posuere. </p>
-                <p><a href='#' class='btn btn-primary' role='button'>Details</a></p>
-            </div>
-        </div>
-    </div>
 
-    
-</div>
-<div class='row'>
-    <div class='col-lg-6'>
+if($result)
+{
+        $totalEvents = $result->num_rows;
+
+        if($totalEvents == 0)
+        {
+            print "<div class='center'><h4>There are currently no scheduled events. Request one below!</h4></div>";
+        }
+        else
+        {
+            //Do math for figuring out the grid column # for the events
+            if($totalEvents >= 4)
+            {
+                $columnNumber = 3;
+            }
+            else
+            {
+                $columnNumber = round(10 / $totalEvents, 0, PHP_ROUND_HALF_DOWN); // Either 11, 5 or 3
+            }
+            //Create the grid for the events. col-md-1 spaces it so it's more centered.
+  
+            print "<div class='events'>
+                   <div class='row margintop15'>
+                   <div class='col-sm-1 col-md-1'>
+                   </div>";
+            $i = 0;
+            while($row = $result->fetch_assoc())
+            {
+                extract($row);
+                if($i == 3) // Maximum 3 events per row
+                {
+                    //Start a new row with same format
+                    print "</div><!--row-->
+                           <div class='row'>
+                           <div class='col-sm-1 col-md-1'>
+                           </div>";
+                    $i = 0;
+                }
+                print "<div class='col-xs-11 col-sm-{$columnNumber} col-md-{$columnNumber}'>
+                    <div class='thumbnail'>
+                        <img class='event-image' src='{$image_link}' alt='...'>
+                        <div class='caption'>
+                            <h3>{$event_name}</h3>
+                            <p>{$description}</p>
+                            <p>{$event_date}</p>
+                            <p>{$event_time}</p>
+                            <p><a href='#' class='btn btn-primary' role='button'>Details</a></p>
+                        </div>
+                    </div>
+                </div>";
+                $i = $i + 1;
+            }
+            print "</div><!--row-->
+                   </div><!--events-->";
+        }
+}
+
+print "<div class='row'>
+    <div class='col-lg-10'>
         <div class='input-group'>
             <div class='input-group-btn'>
                 <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Action <span class='caret'></span></button>
@@ -60,9 +81,11 @@ $result = $db->query($query);
             <input type='text' class='form-control' aria-label='...'>
         </div><!-- /input-group -->
     </div><!-- /.col-lg-6 -->
-</div><!-- /.row -->
-<?php
+</div><!-- /.row -->";
+
 /*
+<?php
+/* phpmailer example
   require_once 'phpmailer/PHPMailerAutoload.php';
   require_once('phpmailer/class.phpmailer.php');
 
@@ -93,4 +116,3 @@ $result = $db->query($query);
   //echo $e->getMessage(); //Error messages from anything else
   } */
 ?>
-</div>
