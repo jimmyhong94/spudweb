28030dd7889c44dbbfbfd53b2ac90e764852bad5
grante1 <grante1@spu.edu>
2015-12-17 20:03:55 -0800

    Added to Project Page
    Added the jumbotron and the modularity project to the page.

--- projects.php
+++ projects.php
@@ -1,2 +1,32 @@
 <?php require_once 'include/header.php'; ?>
 <?php require_once 'include/navigation.php'; ?>
+
+<div class="jumbotron">
+  <h1>SPU DEVELOPERS</h1>
+  <p>Projects</p>
+  <p><a class="btn btn-default btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+
+<div class="middle">
+  <div class="container">
+    <h1 style="font-size:45px;">Projects</h1>
+  </div>
+  <div class="container">
+    <h2>Modularity</h2>
+    <p>
+      One of the projects that we are currently working on is Modularity.
+      Modularity is a voxel based game that we are creating
+      using <a href = "https://www.unrealengine.com/what-is-unreal-engine-4">Unreal Engine 4</a>.
+      The game is called modularity because the player will have a modular weapon that allows them
+      to find, and attach different mods to their  weapon. With these mods the player will be able to
+      create unique weapons with different  capabilities.
+      </p>
+      <p>
+        To view the GitHub repository for this project <a href="https://github.com/SPUDevelopers/Modularity">Click Here!</a><br>
+        To view the Trello for this project <a href="#">Click Here!</a>
+      </p>
+  </div>
+</div>
+
+</body>
+</html>
