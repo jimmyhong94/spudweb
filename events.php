0b5740c27c854db73b5db6ba24ff71e428276861
jimlitical <jimlitical@gmail.com>
2015-12-20 04:43:53 -0800

    Added deletion of events that have past by at least 7 days. Added informational event page.

--- events.php
+++ events.php
@@ -1,94 +1,145 @@
 <?php require_once 'include/header.php'; ?>
 <?php require_once 'include/navigation.php'; ?>
 <?php require_once 'include/databaseobj.inc'; ?>
-
 <?php
+
+// Delete events older than 7 days
 $objDatabase = new DatabaseObj;
 $db = $objDatabase->Open();
-$query = "Select * from events;";
+$query = "SELECT * FROM events WHERE event_date < DATE_SUB(NOW(), INTERVAL 7 DAY);";
 $result = $db->query($query);
-
-if($result)
+if (!empty($result = $db->query($query)))
 {
+    while($row = $result->fetch_assoc()) // Get event data array
+    {   
+        extract($row);
+        echo $event_name;
+        $query = "DELETE FROM events WHERE event_name=\"{$event_name}\" AND description=\"{$description}\"";
+        $db->query($query); // Delete older than 7 days items
+    }
+}
+
+if (isset($_GET['e']))
+{
+    // Show event details here
+    // If it has but hasn't gone past a week still display details without Join button but "Went" button
+    $event = filter_input(INPUT_GET, 'e');
+    $query = "SELECT * FROM Events WHERE event_name=\"{$event}\";";
+    $result = $db->query($query);
+    // Formatting Event NEEDS TO BE DONE STILL
+    if($result->num_rows) // Not a static query so we use num_rows to check if there is content
+    {
+        $row = $result->fetch_assoc();
+        extract($row);
+        print "<img class = 'event-image' src = '{$image_link}' alt = '...'>";
+        print "<div class = 'caption'>";
+        print "<h3>{$event_name}</h3>";
+        print "<p>{$description}</p>";
+        print "<p>{$event_date}</p>";
+        print "<p>{$event_time}</p>";
+        print "<p>Join Button Here</p>";
+    }
+    else
+    {
+        print "<div class = 'jumbotron'>
+                    <h1>This event is invalid.</h1>
+                    <p>Request one below if you have an idea for an event!</p>
+                    </div>";
+    }
+    $objDatabase->Close();
+}
+else
+{
+    $query = "Select * from events;";
+    
+    // Check if query went through successfully
+    // If it didn't it wouldn't go through but this is a static query that we expect to go through
+    if (!empty($result = $db->query($query)))
+    {
         $totalEvents = $result->num_rows;
 
-        if($totalEvents == 0)
+        // If no events are scheduled request ideas
+        if ($totalEvents == 0)
         {
-            print "<div class='jumbotron'>
+            print "<div class = 'jumbotron'>
                     <h1>Currently no events are scheduled.</h1>
-                    <p>Request one below if you have a good event idea!</p>
-                   </div>";
+                    <p>Request one below if you have an idea for an event!</p>
+                    </div>";
         }
         else
         {
             //Do math for figuring out the grid column # for the events
-            if($totalEvents >= 4)
+            if ($totalEvents >= 4)
             {
                 $columnNumber = 3;
-            }
+            } 
             else
             {
                 $columnNumber = round(10 / $totalEvents, 0, PHP_ROUND_HALF_DOWN); // Either 10, 5 or 3
             }
             //Create the grid for the events. col-md-1 spaces it so it's more centered.
-  
-            print "<div class='events'>
-                   <div class='row margintop15'>
-                   <div class='col-sm-1 col-md-1'>
-                   </div>";
+
+            print "<div class = 'events'>";
+            print "<div class = 'row margintop15'>";
+            print "<div class = 'col-sm-1 col-md-1'>";
+            print "</div>";
             $i = 0;
-            while($row = $result->fetch_assoc())
+            while ($row = $result->fetch_assoc())
             {
                 extract($row);
-                if($i == 3) // Maximum 3 events per row
+                if ($i == 3) // Maximum 3 events per row
                 {
                     //Start a new row with same format
-                    print "</div><!--row-->
-                           <div class='row'>
-                           <div class='col-sm-1 col-md-1'>
-                           </div>";
+                    print "</div><!--row-->";
+                    print "<div class = 'row'>";
+                    print "<div class = 'col-sm-1 col-md-1'>";
+                    print "</div>";
                     $i = 0;
                 }
-                print "<div class='col-xs-12 col-sm-{$columnNumber} col-md-{$columnNumber}'>
-                    <div class='thumbnail'>
-                        <img class='event-image' src='{$image_link}' alt='...'>
-                        <div class='caption'>
-                            <h3>{$event_name}</h3>
-                            <p>{$description}</p>
-                            <p>{$event_date}</p>
-                            <p>{$event_time}</p>
-                            <p><a href='#' class='btn btn-primary' role='button'>Details</a></p>
-                        </div>
-                    </div>
-                </div>";
+                print "<div class = 'col-xs-12 col-sm-{$columnNumber} col-md-{$columnNumber}'>";
+                print "<div class = 'thumbnail'>";
+                print "<img class = 'event-image' src = '{$image_link}' alt = '...'>";
+                print "<div class = 'caption'>";
+                print "<h3>{$event_name}</h3>";
+                print "<p>{$description}</p>";
+                print "<p>{$event_date}</p>";
+                print "<p>{$event_time}</p>";
+                print "<p><a href = 'events.php?e={$event_name}' class = 'btn btn-primary' role = 'button'>Details</a></p>";
+                print "</div>";
+                print "</div>";
+                print "</div>";
                 $i = $i + 1;
             }
-            print "</div><!--row-->
-                   </div><!--events-->";
+            print "</div><!--row-->";
+            print "</div><!--events-->";
         }
+
+
+
+        print "<div class = 'row'>";
+        print "<div class = 'col-lg-10'>";
+        print "<div class = 'input-group'>";
+        print "<div class = 'input-group-btn'>";
+        print "<button type = 'button' class = 'btn btn-default dropdown-toggle' data-toggle = 'dropdown' aria-haspopup = 'true' aria-expanded = 'false'>Action <span class = 'caret'></span></button>";
+        print "<ul class = 'dropdown-menu'>";
+        print "<li><a href = '#'>Action</a></li>";
+        print "<li><a href = '#'>Another action</a></li>";
+        print "<li><a href = '#'>Something else here</a></li>";
+        print "<li role = 'separator' class = 'divider'></li>";
+        print "<li><a href = '#'>Separated link</a></li>";
+        print "</ul>";
+        print "</div><!--/btn-group -->";
+        print "<input type = 'text' class = 'form-control' aria-label = '...'>";
+        print "</div><!--/input-group -->";
+        print "</div><!--/.col-lg-6 -->";
+        print "</div><!--/.row -->";
+        
+        $objDatabase->Close();
+    }
 }
-
-print "<div class='row'>
-    <div class='col-lg-10'>
-        <div class='input-group'>
-            <div class='input-group-btn'>
-                <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Action <span class='caret'></span></button>
-                <ul class='dropdown-menu'>
-                    <li><a href='#'>Action</a></li>
-                    <li><a href='#'>Another action</a></li>
-                    <li><a href='#'>Something else here</a></li>
-                    <li role='separator' class='divider'></li>
-                    <li><a href='#'>Separated link</a></li>
-                </ul>
-            </div><!-- /btn-group -->
-            <input type='text' class='form-control' aria-label='...'>
-        </div><!-- /input-group -->
-    </div><!-- /.col-lg-6 -->
-</div><!-- /.row -->";
-
 /*
-<?php
-/* phpmailer example
+  <?php
+  /* phpmailer example
   require_once 'phpmailer/PHPMailerAutoload.php';
   require_once('phpmailer/class.phpmailer.php');
 
