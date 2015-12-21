5bbf139fc43bf178b7ac6fa8c280c6fb8415a7cf
jimlitical <jimlitical@gmail.com>
2015-12-21 05:55:48 -0800

    Added lorem ipsum text. Need to add leaders / contact information.

--- about.php
+++ about.php
@@ -3,5 +3,6 @@
 <div class="middle">
   <div class="container">
     <h2>Hello</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique odio ut diam interdum, feugiat sodales arcu auctor. Suspendisse mattis, nunc nec cursus suscipit, massa ipsum commodo odio, vel malesuada felis nulla ut eros. Donec posuere lacus efficitur elit porttitor pharetra. Aliquam efficitur congue lacus, a pellentesque lacus tincidunt ac. Praesent finibus ultricies pharetra. Curabitur aliquam vehicula sodales. Morbi vel enim magna. Praesent interdum quis dolor id aliquam. Sed eleifend nisl eros, ut euismod purus condimentum euismod. Etiam id aliquam risus, id accumsan nibh. Curabitur bibendum libero nec libero vehicula lacinia. Curabitur sollicitudin est elit, id sollicitudin risus venenatis quis. Curabitur ipsum dui, commodo quis felis et, condimentum auctor diam.</p>
   </div>
 </div>
