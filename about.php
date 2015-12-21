d1f5b23bf5f9e9500fb8e23d3fccc16909bdda4e
jimlitical <jimlitical@gmail.com>
2015-12-21 06:29:06 -0800

    Got a template down for the about page. Need to add contact info (probably emails) and a description (provided by each of us probably). Also need to add a description of the club.

--- about.php
+++ about.php
@@ -1,8 +1,50 @@
 <?php require_once 'include/header.php'; ?>
 <?php require_once 'include/navigation.php'; ?>
 <div class="middle">
-  <div class="container">
-    <h2>Hello</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique odio ut diam interdum, feugiat sodales arcu auctor. Suspendisse mattis, nunc nec cursus suscipit, massa ipsum commodo odio, vel malesuada felis nulla ut eros. Donec posuere lacus efficitur elit porttitor pharetra. Aliquam efficitur congue lacus, a pellentesque lacus tincidunt ac. Praesent finibus ultricies pharetra. Curabitur aliquam vehicula sodales. Morbi vel enim magna. Praesent interdum quis dolor id aliquam. Sed eleifend nisl eros, ut euismod purus condimentum euismod. Etiam id aliquam risus, id accumsan nibh. Curabitur bibendum libero nec libero vehicula lacinia. Curabitur sollicitudin est elit, id sollicitudin risus venenatis quis. Curabitur ipsum dui, commodo quis felis et, condimentum auctor diam.</p>
-  </div>
+    <div class="container">
+        <h2>Hello</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique odio ut diam interdum, feugiat sodales arcu auctor. Suspendisse mattis, nunc nec cursus suscipit, massa ipsum commodo odio, vel malesuada felis nulla ut eros. Donec posuere lacus efficitur elit porttitor pharetra. Aliquam efficitur congue lacus, a pellentesque lacus tincidunt ac. Praesent finibus ultricies pharetra. Curabitur aliquam vehicula sodales. Morbi vel enim magna. Praesent interdum quis dolor id aliquam. Sed eleifend nisl eros, ut euismod purus condimentum euismod. Etiam id aliquam risus, id accumsan nibh. Curabitur bibendum libero nec libero vehicula lacinia. Curabitur sollicitudin est elit, id sollicitudin risus venenatis quis. Curabitur ipsum dui, commodo quis felis et, condimentum auctor diam.</p>
+
+        <div class='about-leaderinfo'>
+            <div class='about-kainoadiv'>
+                <div class='row'>
+                    <div class='col-sm-1 col-md-1'>
+                    </div>
+                <div class='col-sm-4 col-md-4'>
+                    <img class='about-kainoaimg'src='img/kainoa.jpg'></img>
+                </div>
+                <div class='col-sm-4 col-md-4'>
+                    <h4>Kainoa | Club President</h4>
+                    <p>I'm Kainoa.</p>
+                </div>
+                </div>
+            </div>
+            <div class='about-ericdiv'>
+                <div class='row'>
+                    <div class='col-sm-1 col-md-1'>
+                    </div>
+                <div class='col-sm-4 col-md-4'>
+                    <img class='about-ericimg'src='img/ericisacat.jpg'></img>
+                </div>
+                <div class='col-sm-4 col-md-4'>
+                    <h4>Eric | Treasurer</h4>
+                    <p>I'm eric.</p>
+                </div>
+                </div>
+            </div>
+            <div class='about-jimmydiv'>
+                <div class='row'>
+                    <div class='col-sm-1 col-md-1'>
+                    </div>
+                <div class='col-sm-4 col-md-4'>
+                    <img class='about-jimmyimg'src='img/jimmy.jpg'></img>
+                </div>
+                <div class='col-sm-4 col-md-4'>
+                    <h4>Jimmy | Event Coordinator</h4>
+                    <p>I'm Jimmy.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
