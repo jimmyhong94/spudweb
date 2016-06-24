5a6833cb10b4e8f904bae4c757d56498c688a79f
grante1 <grante1@spu.edu>
2016-06-23 17:11:50 -0800

    Add chat page
    Add chat page for the kiwi irc and set up the navigation link.

---
+++ chat.php
@@ -0,0 +1,24 @@
+<?php require_once 'include/header.php'; ?>
+<?php require_once 'include/navigation.php'; ?>
+
+<div class="jumbotron">
+  <h1>SPU DEVELOPERS</h1>
+  <p>The very best of Seattle</p>
+  <p><a class="btn btn-default btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+
+<div class="middle">
+  <div class="container">
+    <iframe src="https://kiwiirc.com/client/irc.slashnet.org/?nick=SPU Dev|?#spud" style="border:0; width:100%; height:450px;"></iframe>
+
+    <h2>Welcom to the Chat</h2>
+    <div class="well">
+      <p>You can chat with ... . I am currently a Senior at Seattle Pacific University pursuing a Bachelor of Science Degree in Computer Science.
+        While studying at Seattle Pacific University, I am also participating in the SPU Developers Club. I am currently a leader in this club and also have the role of
+        Financial Officer.
+      </p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
